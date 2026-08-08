0f8312b2f53c71c027e8ab90bdd5e19150ec6d82
vinay <vinaygadge2003@gmail.com>
2026-08-08 16:10:35 +0530

    UniqueId for Category is implemented

--- src/main/java/com/ecom/project/service/CategoryServiceImpl.java
+++ src/main/java/com/ecom/project/service/CategoryServiceImpl.java
@@ -14,12 +14,21 @@ import java.util.List;
 @Service
 public class CategoryServiceImpl implements CategoryService {
     private List<Category>categories=new ArrayList<>();
+    private long id=1L;
+    // it is not good to have null as id as it may possib;e that user
+    // will not enter the id
+    // id should be handle by application itself
+
     @Override
     public List<Category> getAllCategories() {
         return categories;
     }
     @Override
     public void createCategory(Category category) {
+        category.setCategoryId(id++);
+        // even if user sends id it will get overidden here
+        // so whether sent or not no problem
+        // as we cant expect user to add always a unique id or sometimes he will not
         categories.add(category);
     }
 }
